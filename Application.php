05147e2669beb46f5f84a6e011f156a2bf7f1ec6
Vieru Daniel <daniel_vieru1@yahoo.com>
2023-08-04 10:05:38 +0300

    DatabaseInterface

---
+++ Application.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'DatabaseInterface.php';
+
+class Application
+{
+    public $db;
+    public function __construct(DatabaseInterface  $db)
+    {
+        $this->db=$db;
+    }
+}
---
+++ SqliteDb.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'DatabaseInterface.php';
+
+class SqliteDb implements DatabaseInterface
+{
+    public function getOrders(): array
+    {
+        echo "Calling method" . __METHOD__;
+        return [];
+    }
+
+    public function getOrderById(int $id)
+    {
+        echo "Calling method" . __METHOD__;
+
+    }
+
+    public function createOrder(array $data): bool
+    {
+        echo "Calling method" . __METHOD__ ;
+        // TODO: Implement createOrder() method.
+        return true;
+    }
+
+    public function updateOrder(int $id, array $data): bool
+    {
+        // TODO: Implement updateOrder() method.
+
+        echo "calling method" . __METHOD__;
+        return true;
+    }
+    public function DeleteOrder(int $id): bool
+    {
+        // TODO: Implement updateOrder() method.
+
+        echo "calling method" . __METHOD__;
+        return true;
+    }
+}
